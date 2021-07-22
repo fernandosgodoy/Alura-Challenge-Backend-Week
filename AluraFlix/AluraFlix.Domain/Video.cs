845d4f57976a251fcc43bfa32791ca30f6588967
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-07-22 12:53:07 -0300

    GivenTheProjectEntities_WhenAfterYouWroteTheClass_ThenCheckTheRulesForTheModel passing

--- AluraFlix/AluraFlix.Domain/Video.cs
+++ AluraFlix/AluraFlix.Domain/Video.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AluraFlix.Domain
 {
     public class Video
     {
 
+        [Key]
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
