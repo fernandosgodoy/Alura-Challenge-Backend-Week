e2779ad3a05e9ce64d6f335a33805d629c8d9465
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-07-22 14:57:51 -0300

    created the migration file and updated the database

---
+++ AluraFlix/AluraFlix.EFPersistence/Migrations/20210722175530_TableVideo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AluraFlix.EFPersistence.Migrations
+{
+    public partial class TableVideo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "videos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    titulo = table.Column<string>(type: "varchar(70)", nullable: true),
+                    descricao = table.Column<string>(type: "varchar(400)", nullable: true),
+                    url = table.Column<string>(type: "varchar(70)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_videos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "videos");
+        }
+    }
+}
