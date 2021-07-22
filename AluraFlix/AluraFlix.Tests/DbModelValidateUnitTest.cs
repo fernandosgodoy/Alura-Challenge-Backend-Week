d9af8c1385c714b5065b0849d533d6ace76d1b1b
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-07-22 12:52:16 -0300

    created the Test method GivenTheProjectEntities_WhenAfterYouWroteTheClass_ThenCheckTheRulesForTheModel and validate the model

--- AluraFlix/AluraFlix.Tests/DbModelValidateUnitTest.cs
+++ AluraFlix/AluraFlix.Tests/DbModelValidateUnitTest.cs
@@ -1,14 +1,26 @@
+using AluraFlix.Domain;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace AluraFlix.Tests
 {
     public class DbModelValidateUnitTest
     {
-        //[Fact]
-        //public void Test1()
-        //{
+        [Fact]
+        public void GivenTheProjectEntities_WhenAfterYouWroteTheClass_ThenCheckTheRulesForTheModel()
+        {
+            Assert.True(this.CheckHasDefinedPrimaryKey(typeof(Video)), 
+                "Entity doesn't implement the correct model rules.");
+        }
 
-        //}
+        public bool CheckHasDefinedPrimaryKey(Type source)
+        {
+            return source.GetRuntimeProperties()
+                 .Where(pi => pi.PropertyType == typeof(int)
+                    && pi.GetCustomAttributes<KeyAttribute>(true).Any()).Any();
+        }
     }
 }
