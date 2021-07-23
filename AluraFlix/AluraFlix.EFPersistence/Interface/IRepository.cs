5d88b5c64d8a8cef7bec9d5f570b7c1ccb1f0d8f
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-07-23 10:06:49 -0300

    created the contract interface for repositories

---
+++ AluraFlix/AluraFlix.EFPersistence/Interface/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using AluraFlix.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraFlix.EFPersistence.Interface
+{
+    public interface IRepository<TEntity>
+        where TEntity : EntityBase
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById();
+        int Add(TEntity entity);
+        void Update(TEntity entity);
+        void RemoveById(TEntity entity);
+    }
+}
---
+++ AluraFlix/AluraFlix.Tests/DbPersistenceUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using Moq.AutoMock;
+
+namespace AluraFlix.Tests
+{
+    public class DbPersistenceUnitTest
+    {
+
+        [Fact]
+        public void GivenVideoEntries_WhenYouAreAdding_ThenTryToIncludeTheVideo()
+        {
+            var autoMock = new AutoMocker();
+            //autoMock.CreateInstance<VideoRepository>();
+            //Assert.True(this.CheckHasDefinedPrimaryKey(typeof(Video)),
+            //    "Entity doesn't implement the correct model rules.");
+        }
+
+    }
+}
