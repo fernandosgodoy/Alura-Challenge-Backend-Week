38338d4ea9f239f4ed75f6023137fe6aaddb8497
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2021-07-27 18:11:18 -0300

    created the service for videos

---
+++ AluraFlix/AluraFlix.Services/Applications/VideoService.cs
@@ -0,0 +1,76 @@
+﻿using AluraFlix.Domain;
+using AluraFlix.EFPersistence.Repositories;
+using AluraFlix.Services.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AluraFlix.Services.Applications
+{
+    public class VideoService
+        : IService<Video>
+    {
+
+        private readonly VideoRepository _videoRepository;
+        public VideoService(VideoRepository videoRepository)
+        {
+            this._videoRepository = videoRepository;
+        }
+
+        public bool Delete(int id)
+        {
+            var deleted = false;
+            try
+            {
+                _videoRepository.RemoveById(id);
+                deleted = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return deleted;
+        }
+
+        public Video FindById(int id)
+        {
+            return _videoRepository.GetById(id);
+        }
+
+        public bool Insert(Video entity)
+        {
+            var inserted = false;
+            try
+            {
+                inserted = _videoRepository.Add(entity);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return inserted;
+        }
+
+        public IEnumerable<Video> ListAll()
+        {
+            return _videoRepository.GetAll();
+        }
+
+        public bool Update(Video entity)
+        {
+            var updated = false;
+            try
+            {
+                _videoRepository.Update(entity);
+                updated = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return updated;
+        }
+    }
+}
--- AluraFlix/AluraFlix.Services/Interface/IService.cs
+++ AluraFlix/AluraFlix.Services/Interface/IService.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using AluraFlix.Domain.Base;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AluraFlix.Services.Interface
 {
-    public interface IService
+    public interface IService<TEntity>
+        where TEntity : EntityBase
     {
+        IEnumerable<TEntity> ListAll();
+        TEntity FindById(int id);
+        bool Insert(TEntity entity);
+        bool Update(TEntity entity);
+        bool Delete(int id);
     }
 }
